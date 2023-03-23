bc59a7758eaa804b6c3229e6da600335bb7e96b0
Juan Marino <juan.marino@fedex.com>
2023-03-23 07:34:20 -0500

    started making changes to the app.js, dynamic html rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react"; //for class component
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//class component approach
+class App extends Component {
+	//when something is intantiated, first run constructur
+	constructor() {
+		super();
+
+		this.state = {
+			monsters: [
+				{
+					id: 1,
+					name: "Linda",
+				},
+				{
+					id: 2,
+					name: "Frank",
+				},
+				{
+					id: 3,
+					name: "Jacky",
+				},
+			],
+		};
+	}
+
+	render() {
+		return (
+			<div className="App">
+				{this.state.monsters.map((monster) => {
+					//key value is something react uses to make it optimizedand quick. uses key value to associate changes
+					return <h1 key={monster.id}>{monster.name}</h1>;
+				})}
+			</div>
+		);
+	}
 }
 
 export default App;
