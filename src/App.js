3de0bdcd18a3a4fa43d314a2716ad608481ca185
Juan Marino <juan.marino@fedex.com>
2023-03-24 10:39:28 -0500

    used funcional approach to achieve same functionality, app is finished

--- src/App.js
+++ src/App.js
@@ -1,72 +1,118 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react"; //for class component
+import { useState, useEffect } from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
-//class component approach
-class App extends Component {
-	//when something is intantiated, first run constructur
-	constructor() {
-		super();
+//*****************functional component approach****************
+const App = () => {
+	//props go in ()
+	//hooks. initializing the states
+	const [searchField, setSearchField] = useState(""); //[value,setValue]
+	const [monsters, setMonsters] = useState([]);
+	const [filteredMonsters, setFilterMonsters] = useState(monsters);
+	const [stringField, setStringField] = useState("");
 
-		//initializing state object
-		this.state = {
-			monsters: [],
-			searchField: "", //adding a new property that keeps track of the search field and is accessible everywhere in the app
-		};
-	}
-
-	//mounting is the first time a component gets dom rednedered. First time only
-	componentDidMount() {
+	//to avoid infinite re render
+	useEffect(() => {
 		fetch("https://jsonplaceholder.typicode.com/users") //getting the data from the API
 			.then(
 				(response) =>
 					//promise approach
 					response.json() //parsing the json into objects
 			)
-			.then((users) =>
-				this.setState(
-					() => {
-						return { monsters: users }; //setting the array to the data from api
-					},
-					() => {
-						//executes after
-						console.log(this.state);
-					}
-				)
-			);
-	}
+			.then((users) => setMonsters(users));
+	}, []); //this will run only if the values in the dependency(array) have changed. empty means will only run once
 
-	onSearchChange = (event) => {
-		const searchField = event.target.value.toLocaleLowerCase();
-		this.setState(() => {
-			return { searchField: searchField }; //equal to searchField:searchField
-		});
-	};
-
-	//constructor runs first, then render, then mount, then render again because mount changed the state
-	render() {
-		//do this to avoid using this everwhere
-		const { monsters, searchField } = this.state;
-		const { onSearchChange } = this;
-
-		const filteredMonsters = monsters.filter((monster) => {
+	useEffect(() => {
+		const newFilteredMonsters = monsters.filter((monster) => {
 			return monster.name.toLocaleLowerCase().includes(searchField);
 		});
 
-		return (
-			<div className="App">
-				<h1 className="app-title">Monsters Rolodex</h1>
-				<SearchBox
-					onChangeHandler={onSearchChange}
-					placeholder="search monsters"
-					className="search-box"
-				/>
-				<CardList monsters={filteredMonsters} />
-			</div>
-		);
-	}
-}
+		setFilterMonsters(newFilteredMonsters);
+	}, [monsters, searchField]); //run the effect when array changes or search field changes
+	const onSearchChange = (event) => {
+		const searchFieldString = event.target.value.toLocaleLowerCase();
+		setSearchField(searchFieldString);
+	};
+
+	return (
+		<div className="App">
+			<h1 className="app-title">Monsters Rolodex</h1>
+			<SearchBox
+				onChangeHandler={onSearchChange}
+				placeholder="search monsters"
+				className="search-box"
+			/>
+			<CardList monsters={filteredMonsters} />
+		</div>
+	);
+};
+
+// //**********************class component approach*********************
+// class App extends Component {
+// 	//when something is intantiated, first run constructur
+// 	constructor() {
+// 		super();
+
+// 		//initializing state object
+// 		this.state = {
+// 			monsters: [],
+// 			searchField: "", //adding a new property that keeps track of the search field and is accessible everywhere in the app
+// 		};
+// 	}
+
+// 	//mounting is the first time a component gets dom rednedered. First time only
+// 	componentDidMount() {
+// 		fetch("https://jsonplaceholder.typicode.com/users") //getting the data from the API
+// 			.then(
+// 				(response) =>
+// 					//promise approach
+// 					response.json() //parsing the json into objects
+// 			)
+// 			.then((users) =>
+// 				this.setState(
+// 					() => {
+// 						return { monsters: users }; //setting the array to the data from api
+// 					},
+// 					() => {
+// 						//executes after
+// 						console.log(this.state);
+// 					}
+// 				)
+// 			);
+// 	}
+
+// 	onSearchChange = (event) => {
+// 		const searchField = event.target.value.toLocaleLowerCase();
+// 		this.setState(() => {
+// 			return { searchField: searchField }; //equal to searchField:searchField
+// 		});
+// 	};
+
+// 	//constructor runs first, then render, then mount, then render again because mount changed the state
+// 	render() {
+// 		//do this to avoid using this everwhere
+// 		const { monsters, searchField } = this.state;
+// 		const { onSearchChange } = this;
+
+// 		const filteredMonsters = monsters.filter((monster) => {
+// 			return monster.name.toLocaleLowerCase().includes(searchField);
+// 		});
+
+// 		return (
+// 			<div className="App">
+// 				<h1 className="app-title">Monsters Rolodex</h1>
+// 				<SearchBox
+// 					onChangeHandler={onSearchChange}
+// 					placeholder="search monsters"
+// 					className="search-box"
+// 				/>
+// 				<CardList monsters={filteredMonsters} />
+// 			</div>
+// 		);
+// 	}
+// }
 
 export default App;
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,18 +2,31 @@ import { Component } from "react";
 import "./card-list.styles.css";
 import Card from "../card/card.component";
 
-class CardList extends Component {
-	render() {
-		//console.log(this.props); //props show a key (name of prop) and value(whatever you passed)
-		const { monsters } = this.props;
-		return (
-			<div className="card-list">
-				{monsters.map((monster) => {
-					return <Card monster={monster} />;
-				})}
-			</div>
-		);
-	}
-}
+//function appraoch, the destructoring can happen in the props section
+const CardList = ({ monsters }) => {
+	return (
+		//mno return statement is implicit return
+		<div className="card-list">
+			{monsters.map((monster) => {
+				return <Card monster={monster} key={monster.id} />;
+			})}
+		</div>
+	);
+};
+
+//class approach
+// class CardList extends Component {
+// 	render() {
+// 		//console.log(this.props); //props show a key (name of prop) and value(whatever you passed)
+// 		const { monsters } = this.props;
+// 		return (
+// 			<div className="card-list">
+// 				{monsters.map((monster) => {
+// 					return <Card monster={monster} />;
+// 				})}
+// 			</div>
+// 		);
+// 	}
+// }
 
 export default CardList;
